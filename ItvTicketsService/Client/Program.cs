0ca716c243afa66461a9688f6c74ff0c85376df5
Ritesh Ravlani <ritesh142@gmail.com>
2021-09-29 19:19:45 +0530

    pdf

--- ItvTicketsService/Client/Program.cs
+++ ItvTicketsService/Client/Program.cs
@@ -1,15 +1,11 @@
 using ItvTicketsService.Client.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using MudBlazor.Services;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 
 namespace ItvTicketsService.Client
